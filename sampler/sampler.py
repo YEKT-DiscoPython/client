b0f5594babd8cc4d1f825b246b4056b6e26565cd
InHavk <inhavk@gmail.com>
2014-11-13 02:19:41 +0500

    Попытки записи и воспроизведения Ну и еще какие-то там изменения

--- sampler/sampler.py
+++ sampler/sampler.py
@@ -3,27 +3,50 @@ import pygame
 from button import Button
 from os import getcwd
 from button_style import RED, BLUE, GREEN, BLACK, WHITE, ORANGE, BUTTON_STYLE_1
+import time
 
 SAMPLES_PATH = getcwd() + "/"
+config_and_status = {"record": False}
+db_samples = {}
 
-def load_json(path):
+mode = 0
+
+def load_db(path):
+    global db_samples
+    db_samples = json.loads(open(path).read())
+
+def load_keys(path):
     global is_playing
     config = json.loads(open(path).read())
     for key, sample in config.items():
-        samples[key] = pygame.mixer.Sound(SAMPLES_PATH + sample)
+        samples[key] = pygame.mixer.Sound(SAMPLES_PATH + db_samples[sample])
     is_playing = {k: False for k in config.keys()}
 
 def quit_it():
     pygame.quit()
 
+def get_time():
+    return int(time.time() * 10000)
+
 def start_record():
+    global notes
+    global start_record_time
+    notes = []
+    start_record_time = get_time()
     #TODO Start record
+    config_and_status['record'] = True
     buttons["record"].function = stop_record
     buttons["record"].text = "Stop Record"
     buttons["record"].render_text()
 
 def stop_record():
+    global start_record_time
+    global mode
+    start_record_time = -1
+    temp_notes.clear()
+    mode = 1
     #TODO Stop record
+    config_and_status['record'] = False
     buttons["record"].function = start_record
     buttons["record"].text = "Start Record"
     buttons["record"].render_text()
@@ -44,34 +67,63 @@ buttons["record"] = Button((0,0,80,20),RED, start_record,
                  text="Start Record", **BUTTON_STYLE_1)
 buttons["record"].rect.center = (screen.get_rect().centerx,10)
 
+clock = pygame.time.Clock()
+start_record_time = -1
+notes = []
+temp_notes = {}
 
 samples =  {}
 is_playing = {}
-load_json('test.json')
-
-global channels
-channels = [pygame.mixer.Channel(i) for i in range(8)]
+load_db('db_samples.json')
+load_keys('test.json')
 
 while True:
-    event = pygame.event.wait()
+    clock.tick(1000)
+    if mode == 0:
+        event_time = get_time()
+        event = pygame.event.poll()
+        if event.type != pygame.NOEVENT:
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
-    if event.type == pygame.QUIT:
-        pygame.quit()
+            if event.type in (pygame.KEYDOWN, pygame.KEYUP):
+                key = pygame.key.name(event.key)
 
-    if event.type in (pygame.KEYDOWN, pygame.KEYUP):
-        key = pygame.key.name(event.key)
+            if event.type == pygame.KEYDOWN:
+                if (key in samples.keys()) and (not is_playing[key]):
+                    if start_record_time != -1:
+                        temp_notes[key] = event_time
+                    samples[key].play(fade_ms=50)
+                    is_playing[key] = True
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+            elif event.type == pygame.KEYUP and key in samples.keys():
+                if start_record_time != -1:
+                    notes.append((event_time - start_record_time, key, event_time - temp_notes[key]))
+                samples[key].fadeout(50)
+                is_playing[key] = False
 
-    if event.type == pygame.KEYDOWN:
-        if (key in samples.keys()) and (not is_playing[key]):
-            samples[key].play(fade_ms=50)
-            is_playing[key] = True
-        elif event.key == pygame.K_ESCAPE:
-            pygame.quit()
-    elif event.type == pygame.KEYUP and key in samples.keys():
-        samples[key].fadeout(50)
-        is_playing[key] = False
+            buttons_update(event, buttons)
+            pygame.display.update()
+    elif mode == 1:
+        if start_record_time == -1:
+                start_record_time = get_time()
 
-    buttons_update(event, buttons)
-    pygame.display.update()
+        tick_time = get_time() - start_record_time
+
+        if len(temp_notes) > 0:
+            for i in list(temp_notes.keys()):
+                if temp_notes[i] <= tick_time:
+                    samples[i].fadeout(50)
+                    temp_notes.pop(i)
+
+        if len(notes) > 0:
+            if notes[0][0] <= tick_time:
+                note = notes.pop(0)
+                temp_notes[note[1]] = note[0] + note[2] #Set stop time
+                samples[note[1]].play(fade_ms=50)
+
+        if len(notes) == 0 and len(temp_notes) == 0:
+            mode = 0
 
 pygame.quit()
